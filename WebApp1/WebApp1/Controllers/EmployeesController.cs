5aeb1385ee130b509609df4cc6b3de60de05537a
geethasamynathan <geethasamynathan2011@gmail.com>
2022-03-23 11:43:35 +0530

    Day3 MVC Demo1 Added

---
+++ WebApp1/WebApp1/Controllers/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            List<Employee> emplist = new List<Employee>() {
+             new Employee() { Empid = 101, Name = "Mohan", Gender = "Male", Salary = 230000 },
+             new Employee() { Empid = 102, Name = "Vishwas", Gender = "Male", Salary = 240000 },
+             new Employee() { Empid = 103, Name = "Pavani", Gender = "Female", Salary = 270000 },
+            new Employee() { Empid = 104, Name = "Nethra", Gender = "Female", Salary = 290000 },
+
+             };
+            //ViewData["emp"] = employee;
+           // ViewBag.emp1 = employee;
+            //ViewData["empid"] = "101";
+
+            //ViewData["empname"] = "Geetha";
+            //ViewData["gender"] = "Female";
+
+            //ViewBag.eid = 102;
+            //ViewBag.ename = "Raju";
+            //ViewBag.gendr = "male";
+
+            
+
+            return View(emplist);
+        }
+    }
+}
---
+++ WebApp1/WebApp1/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp1.Models
+{
+    public class Employee
+    {
+        public int Empid { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+    }
+}
