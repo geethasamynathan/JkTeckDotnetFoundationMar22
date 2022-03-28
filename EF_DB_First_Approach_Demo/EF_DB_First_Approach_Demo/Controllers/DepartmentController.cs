83ac09ecf6b654ed2801682fd9411bf6cb33791a
geethasamynathan <geethasamynathan2011@gmail.com>
2022-03-28 15:06:30 +0530

    Departement CRUD Done

--- EF_DB_First_Approach_Demo/EF_DB_First_Approach_Demo/Controllers/DepartmentController.cs
+++ EF_DB_First_Approach_Demo/EF_DB_First_Approach_Demo/Controllers/DepartmentController.cs
@@ -9,11 +9,57 @@ namespace EF_DB_First_Approach_Demo.Controllers
 {
     public class DepartmentController : Controller
     {
+
+        JK_Tech_DBEntities entities = new JK_Tech_DBEntities();
         // GET: Department
         public ActionResult Index()
-        {
-            JK_Tech_DBEntities entities = new JK_Tech_DBEntities();          
+        {      
             return View(entities.Departments.ToList());
+        }
+     
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Department department)
+        {
+            entities.Departments.Add(department);
+            entities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            Department dept = entities.Departments.Find(id);
+            return View(dept);
+        }
+        [HttpPost]
+        public ActionResult Edit(Department department)
+        {
+
+            entities.Departments.Attach(department);
+            entities.Entry(department).State = System.Data.Entity.EntityState.Modified;
+            entities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            Department dept1 = entities.Departments.Find(id);
+            return View(dept1);
+        }
+        public ActionResult Delete(int id)
+        {
+            Department dept = entities.Departments.Find(id);
+
+            return View(dept);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id,Department department)
+        {
+            Department dept = entities.Departments.Find(id);
+            entities.Departments.Remove(dept);           
+            entities.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
