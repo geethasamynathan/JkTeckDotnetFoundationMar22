db4aae6ecd2f53b061cf4871dc89dcbe548e6f13
geethasamynathan <geethasamynathan2011@gmail.com>
2022-04-06 09:32:00 +0530

    ChildAction and Outputcahce demo added

---
+++ ChildActionOnlyAttributeDemo/ChildActionOnlyAttributeDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChildActionOnlyAttributeDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //[ChildActionOnly]       
+        public ActionResult Countries(List<string> countryData)
+        {
+            return View(countryData);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
