5c80f9639c837e4f4301116a9ccfa094666e1ba0
geethasamynathan <geethasamynathan2011@gmail.com>
2022-04-07 15:44:15 +0530

    WEb APi intro and demo added

---
+++ EmployeeService/EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeService.Models;
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using(EmployeeContext  context=new EmployeeContext())
+            {
+                return context.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using(EmployeeContext context=new EmployeeContext())
+            {
+                return context.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
